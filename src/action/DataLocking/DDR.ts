927e7a2de755768ed9acac6ec92af1c733181320
chinhtt <chinhtt@vmnodev.com>
2022-12-20 15:54:37 +0700

    update getAllDDR

--- src/action/DataLocking/DDR.ts
+++ src/action/DataLocking/DDR.ts
@@ -232,11 +232,11 @@ export class DDR {
       CONFIG.ClaimHolder.abi,
       patientDID
     );
-    
+
     let tokenIds = await this.ddr.methods.getListDDRTokenIdOfPatient(patientDID).call();
     let ddrs = Array<any>();
     for (let i = 0; i < tokenIds.length; i++) {
-      let ddr = await this.ddr.methods.getToken(i).call();
+      let ddr = await this.ddr.methods.getToken(tokenIds[i]).call();
       ddrs.push(ddr);
     }
     return ddrs;
